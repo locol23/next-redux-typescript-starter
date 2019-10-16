669a55e319ba067a6188576a0fb65ae7beefa675
Yoshitaka Terazawa <locol6622@gmail.com>
2019-10-16 16:43:30 +0900

    chore(deps): remove unnecessary package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-const withTypescript = require('@zeit/next-typescript')
-
 const config = {
   target: process.env.NODE_ENV === 'production' ? 'serverless' : 'server',
 }
 
-module.exports = withTypescript(config)
+module.exports = config
